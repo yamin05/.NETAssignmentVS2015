79537d84c7ae8b78dc962d8537f5c79c24fcc6c6
Qing <Qing@DESKTOP-9PS38MN>
2017-04-05 19:52:53 +1000

    Revert "qing"
    This reverts commit 63ab5674c125b3b6f17fc42e46f8e1ce98f14c62.

--- Assignment1.Tests/UnitTestforVerify.cs
+++ Assignment1.Tests/UnitTestforVerify.cs
@@ -7,7 +7,72 @@ namespace Assignment1.Tests
     [TestClass]
     public class VerifyTest
     {
-        
+        /* From Qing */
+        //1
+            Verify _v;
+
+            [TestInitialize]
+            public void Setup()
+            {
+                Verify _v = new Verify();
+            }
+
+            //2 check login info same or not
+            [TestMethod()]
+            public void LoginInfo(string name, string pass)
+            {
+                name = "Name";
+                pass = "pass";
+
+                Assert.AreEqual(name, _v.Username);
+                Assert.AreEqual(pass, _v.Password);
+                
+            }
+
+            //3 check username and password not null
+            [TestMethod()]
+            public void LoginInfonotNull()
+            {
+                string Nname = _v.Username;
+                string Npass = _v.Password;
+
+                Assert.IsNull(Nname);
+                Assert.IsNull(Npass);
+            }
+
+            //4 check login function
+            [TestMethod()]
+            public void LoginTest()
+            {
+                string username = "Name1";
+                string password = "Password1";
+
+                Assert.IsTrue(_v.Login(username, password));
+
+            }
+
+            //5 check return report info
+            [TestMethod()]
+            public void reportTest()
+            {
+                string reportinput = "input";
+                string EreportOutput = "Output";
+                string AreportOutput = _v.OutReport(reportinput);
+
+                Assert.AreSame(EreportOutput, AreportOutput);
+            }
+
+            //6 check add new client function
+            [TestMethod()]
+            public void AddclientTest()
+            {
+                string Clientinfo ="info";
+                string EResult = "result";
+                string ClientResult = _v.AddClient(Clientinfo);
+
+                Assert.AreSame(EResult, ClientResult);
+            }
+
 
         [TestMethod]
         public void VerifyUsername()
