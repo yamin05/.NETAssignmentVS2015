63ab5674c125b3b6f17fc42e46f8e1ce98f14c62
Qing <Qing@DESKTOP-9PS38MN>
2017-04-05 19:52:07 +1000

    qing

---
+++ Assignment1.Tests/Utest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1;
+
+namespace Assignment1.Tests
+{
+    [TestClass]
+    public class W2Tests
+    {
+
+        [TestClass]
+        public class W2Test
+        {
+            Game _game;
+
+
+            public TestContext TestContext { get; set; }
+
+            //1
+            [TestInitialize]
+            public void Setup()
+            {
+                _game = new Game();
+            }
+
+            //2 check login info same or not
+            [TestMethod()]
+            public void LoginInfo(string name, string pass)
+            {
+                name = "Name";
+                pass = "pass";
+
+                Assert.AreNotEqual(name, _game.Username);
+                Assert.AreNotEqual(pass, _game.Password);
+
+            }
+
+            //3 check username and password not null
+            [TestMethod()]
+            public void LoginInfonotNull()
+            {
+                string Nname = _game.Username;
+                string Npass = _game.Password;
+
+                Assert.IsNotNull(Nname);
+                Assert.IsNotNull(Npass);
+            }
+
+            //4 check login function
+            [TestMethod()]
+            public void LoginTest()
+            {
+                string username = "Name1";
+                string password = "Password1";
+
+                Assert.IsTrue(_game.Login(username, password));
+
+            }
+
+            //5 check return report info
+            [TestMethod()]
+            public void reportTest()
+            {
+                string reportinput = "input";
+                string EreportOutput = "Output";
+                string AreportOutput = _game.OutReport(reportinput);
+
+                Assert.AreNotSame(EreportOutput, AreportOutput);
+            }
+
+            //6 check add new client function
+            [TestMethod()]
+            public void AddclientTest()
+            {
+                string Clientinfo = "info";
+                string EResult = "result";
+                string ClientResult = _game.AddClient(Clientinfo);
+
+                Assert.AreNotSame(EResult, ClientResult);
+            }
+
+            
+
+
+        }
+    }
+}
---
+++ WebApplication1/W2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Game
+    {
+        
+        public String Username
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public String Password
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public bool Login(string Uname, string Pword)
+        {
+            return true;
+        }
+
+        public string OutReport(string input)
+        {
+            string output = "";
+            return output;
+        }
+
+        public string AddClient(string info)
+        {
+            string intervention1 = "info";
+            return intervention1;
+        }
+    }
+}
