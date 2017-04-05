bffb2831575a36d9b625a194fdb747a11c1f7c3d
Yuxiang <Yuxiang@ZYX>
2017-04-05 18:28:45 +1000

    Create verification and testing for username, name and the site engineers limition.

---
+++ Assignment1.Tests/UnitTestforVerify.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using WebApplication1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment1.Tests
+{
+    [TestClass]
+    public class VerifyTest
+    {
+
+        [TestMethod]
+        public void VerifyUsername()
+        {
+            string username = "admin";
+            bool verifyFlag = true;
+            if (!Verify.VerifyUsername(username))
+            {
+                verifyFlag = false;
+            }
+            bool expected = true;
+            Assert.AreEqual(expected.ToString(), verifyFlag.ToString());
+        }
+
+        [TestMethod]
+        public void VerifyName()
+        {
+            string lastname = "Zhang";
+            string firstname = "Yuxiang";
+            bool verifyFlag = true;
+            if (!Verify.VerifyName(firstname, lastname))
+            {
+                verifyFlag = false;
+            }
+            bool expected = true;
+            Assert.AreEqual(expected.ToString(), verifyFlag.ToString());
+        }
+
+        [TestMethod]
+        public void VerifyEngineerLimit()
+        {
+            decimal interventionhour = 50;
+            decimal interventioncost = 2000;
+            bool verifyFlag = true;
+            if (!Verify.VerifyEngineerLimit(interventionhour, interventioncost))
+            {
+                verifyFlag = false;
+            }
+            bool expected = true;
+            Assert.AreEqual(expected.ToString(), verifyFlag.ToString());
+        }
+
+
+
+    }
+}
---
+++ WebApplication1/Verify.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace WebApplication1
+{
+    public class Verify
+    {
+        public static bool VerifyUsername(string username)
+        {
+            if (username != "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool VerifyName(string firstname, string lastname)
+        {
+            if (firstname != "" && lastname != ""
+                && Regex.IsMatch(firstname, "^[ A-Za-z]*$")
+                && Regex.IsMatch(lastname, "^[ A-Za-z]*$"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static bool VerifyEngineerLimit(decimal interventionhour, decimal interventioncost)
+        {
+            if (interventionhour > 50 || interventioncost > 2000)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
