c45f09781269b28e7c6d03cb233bd2f448124272
divasmalik <divasmalik22@gmail.com>
2017-04-05 19:26:20 +1000

    merge

--- Assignment1.Tests/UnitTestforVerify.cs
+++ Assignment1.Tests/UnitTestforVerify.cs
@@ -115,7 +115,50 @@ namespace Assignment1.Tests
             Assert.AreEqual(expected.ToString(), verifyFlag.ToString());
         }
 
+        [TestClass]
+        public class PasswordchangerTest
+        {
+            public TestContext TestContext { get; set; }
 
 
+            [TestMethod()]
+            [ExpectedException(typeof(ArgumentException))]
+            public void OldPassWrong()
+            {
+                PasswordChanger Pass1 = new PasswordChanger();
+                String OldPassword = "Divas";
+                String Password = "Diva";
+                String NewPassword = "Malik";
+                String ConfirmPassword = "Malik";
+                Pass1.ChangePassword(NewPassword, OldPassword, Password, ConfirmPassword);
+            }
+
+            [TestMethod()]
+            public void OldPassEmpty()
+            {
+                PasswordChanger Pass1 = new PasswordChanger();
+                String OldPassword = "";
+                String Password = "Divas";
+                String NewPassword = "Malik";
+                String ConfirmPassword = "Malik";
+                Pass1.ChangePassword(NewPassword, OldPassword, Password, ConfirmPassword);
+                Assert.IsNull(Pass1.oldPassword);
+            }
+
+            [TestMethod()]
+            public void ConfirmPassDoesntMatchNewPass()
+            {
+                PasswordChanger Pass1 = new PasswordChanger();
+                String ConfirmPassword = "Mal";
+                String NewPassword = "Malik";
+                Pass1.ConfirmPass(NewPassword, ConfirmPassword);
+                Assert.AreEqual(0, Pass1.flag);
+            }
+
+
+
+        }
     }
+
 }
+
