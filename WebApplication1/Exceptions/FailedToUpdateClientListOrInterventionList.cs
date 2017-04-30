2cec53aae2c15f1bb6ff190968dd756486c1f919
Qing <Qing@DESKTOP-9PS38MN>
2017-04-30 23:50:33 +1000

    some exception files and one validation for quality management

---
+++ WebApplication1/Exceptions/FailedToUpdateClientListOrInterventionList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Exceptions
+{
+    public class FailedToUpdateClientListOrInterventionList : Exception
+    {
+        private int errorCode { get; set; }
+        private string errorMessage { get; set; }
+
+        public FailedToUpdateClientListOrInterventionList()
+        {
+            errorCode = 114;
+            errorMessage = "Failed to Update client/Intervention list";
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return errorMessage;
+            }
+        }
+    }
+}
