0eb270591f7216727d7257ddcf5c6d15e389c067
divasmalik <divasmalik22@gmail.com>
2017-05-03 17:19:21 +1000

    Changes

--- Assignment1.Tests/ListInterventionTests.cs
+++ Assignment1.Tests/ListInterventionTests.cs
@@ -17,22 +17,21 @@ namespace Assignment1.Tests
         [TestMethod]
         public void ListInterventionsHelper()
         {
-            Mock<Users> MockUser = new Mock<Users>();
-            Mock<ListInterventionForManager> Intervention1 = new Mock<ListInterventionForManager>();
+            Users MockUser = new Users();
+            ListInterventionForManager Intervention1 = new ListInterventionForManager();
             List<ListInterventionForManager> interventionListMock = new List<ListInterventionForManager>();
-            MockUser.Object.District = Convert.ToInt32(Districts.Sydney);
-            MockUser.Object.MaximumCost = 200;
-            MockUser.Object.MaximumHours = 10;
-            Intervention1.Object.InterventionCost = 180;
-            Intervention1.Object.InterventionHours = 10;
-            Intervention1.Object.District = Convert.ToInt32(Districts.Sydney);
-            interventionListMock.Add(Intervention1.Object);
+            MockUser.District = Convert.ToInt32(Districts.Sydney);
+            MockUser.MaximumCost = 200;
+            MockUser.MaximumHours = 10;
+            Intervention1.InterventionCost = 180;
+            Intervention1.InterventionHours = 10;
+            Intervention1.District = Convert.ToInt32(Districts.Sydney);
+            interventionListMock.Add(Intervention1);
             var list2 = interventionListMock;
             var list = new ListInterventionsHelper();
-            var list1= list.ValidateProposedInterventions(MockUser.Object, interventionListMock);
+            var list1= list.ValidateProposedInterventions(MockUser, interventionListMock);
             Collection<ListInterventionForManager> collection = new Collection<ListInterventionForManager>(list1);
             CollectionAssert.AreEqual(collection, list2);
-
         }
     }
 }
